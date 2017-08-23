959f1cf4495e79fd29df3a2e7389cfd9bea96865
saigach <saigach@gmail.com>
2017-08-23 14:50:02 +0500

    Accepts data array from POST, forms hardcoded

--- app.py
+++ app.py
@@ -14,25 +14,30 @@ def index():
 @app.route("/inflect", methods=['GET', 'POST'])
 def inflect():
     if request.method == 'POST':
-        params = request.form
+        phrases = request.form.getlist('phrase')
+        codes = request.form.getlist('code')
+        params = dict(zip(codes, phrases))
     else:
         params = request.args
 
-    if 'phrase' not in params:
-        return u'укажите слово', 400,  {'Content-Type':'text/plain; charset=utf-8'}
-    if 'forms' not in params and 'cases' not in params :
-        return u'выберите падежи или/и числа', 400,  {'Content-Type':'text/plain; charset=utf-8'}
+    #if 'phrase' not in params:
+    #    return u'укажите слово', 400,  {'Content-Type':'text/plain; charset=utf-8'}
+    #if 'forms' not in params and 'cases' not in params :
+    #    return u'выберите падежи или/и числа', 400,  {'Content-Type':'text/plain; charset=utf-8'}
 
-    phrase = params['phrase']
-    form_sets = params.getlist('forms') if params.getlist('forms') else params.getlist('cases')
+    #phrase = params['phrase']
+    #form_sets = params.getlist('forms') if params.getlist('forms') else params.getlist('cases')
+    form_sets = ['gent', 'accs']
 
-
-    morph = pymorphy2.MorphAnalyzer()
-    inflector = PhraseInflector(morph)
-    result = {'orig': phrase}
-    for forms_string in form_sets:
-        form_set = set(forms_string.split(',')) & set(GRAM_CHOICES)
-        result[forms_string] = inflector.inflect(phrase, form_set)
+    result = {}
+    for (code, phrase) in params.items():
+        morph = pymorphy2.MorphAnalyzer()
+        inflector = PhraseInflector(morph)
+        result2 = {'orig': phrase}
+        for forms_string in form_sets:
+            form_set = set(forms_string.split(',')) & set(GRAM_CHOICES)
+            result2[forms_string] = inflector.inflect(phrase, form_set)
+        result[code] = result2
     return json.dumps(result), 200, {'Content-Type':'text/json; charset=utf-8'}
 
 if __name__ == "__main__":
